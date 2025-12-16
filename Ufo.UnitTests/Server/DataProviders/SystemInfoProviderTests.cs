de60b643ed5e4209787c7e0c9a333c12b35b76c7
Alex L <LA.xdna@gmail.com>
2025-12-16 04:26:57 +0100

    commented SystemInfoProviderTests

--- Ufo.UnitTests/Server/DataProviders/SystemInfoProviderTests.cs
+++ Ufo.UnitTests/Server/DataProviders/SystemInfoProviderTests.cs
@@ -8,19 +8,20 @@ public class SystemInfoProviderTests : BaseTest
 {
     private readonly SystemInfoProvider _sut = new SystemInfoProvider();
 
-    [Fact]
-    public async Task GetSystemInformation_Test()
-    {
-        var path1 = "c:\\logs";
-        var path2 = "d:\\Tmp";
+    // TODO LA: Implement tests for SystemInfoProvider
+    //[Fact]
+    //public async Task GetSystemInformation_Test()
+    //{
+    //    var path1 = "c:\\logs";
+    //    var path2 = "d:\\Tmp";
 
-        var result1 = _sut.GetSystemInformation(path1);
-        var result2 = _sut.GetSystemInformation(path2);
+    //    var result1 = _sut.GetSystemInformation(path1);
+    //    var result2 = _sut.GetSystemInformation(path2);
 
-        var json1 = JsonConvert.SerializeObject(result1);
-        var json2 = JsonConvert.SerializeObject(result2);
+    //    var json1 = JsonConvert.SerializeObject(result1);
+    //    var json2 = JsonConvert.SerializeObject(result2);
 
 
-        result1.Should().NotBe(result2);
-    }
+    //    result1.Should().NotBe(result2);
+    //}
 }
